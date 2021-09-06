4db0319e682e8d180c9dd089705031cb622cf49f
amber-ii <lube8262@gmail.com>
2021-09-06 09:53:25 +0800

    修改pt大小

--- public/water.js
+++ public/water.js
@@ -9,7 +9,7 @@ function onMessageArrived(r_message) {
         var waterValue = rcmsg[key];
         if (document.getElementById(waterId) != null) {
             document.getElementById(waterId).innerHTML = waterValue;
-            
+
             if (document.getElementById('Sw1').innerHTML == 1) {
                 // document.getElementById('motor1').style.color = 'black'
                 document.getElementById('motor1').setAttribute('class', 'motor-run');
@@ -18,68 +18,38 @@ function onMessageArrived(r_message) {
                 document.getElementById('motor2').setAttribute('class', 'motor-run');
 
             }
-           
+
         }
     }
 }
 
 
 
-function send_message(airId) {
+function modPress() {
 
     if (connected_flag == 0) {
         out_msg = "Not Connected so can't send"
         console.log(out_msg);
+        // alert(out_msg);
         return false;
     }
-    var msg = `{"${airId}":` + changeState(airId) + "}";
-    console.log(msg);
-    message = new Paho.MQTT.Message(msg);
-    message.destinationName = subTopic + "Conf";//in arduino
-    mqtt.send(message);
-    return false;
-}
 
-
-
-function changeState(airId) {
-
-    if (airId.includes('fc')) {
-
-        if (document.getElementById(airId).getAttribute('class') == "snow") {
-            var fc_result = confirm(`確定要關閉冷氣?`);
-            if (fc_result) {
-                document.getElementById(airId).style.animationPlayState = "paused";
-                document.getElementById(airId).style.color = "gray";
-                return 0;
-            } else {
-                return 1;
-            }
-        } else {
-            document.getElementById(airId).style.animationPlayState = "running";
-            document.getElementById(airId).style.color = "#61DAFB"; //因為會延遲而設置(障眼用)
-            return 1;
-        }
-
-    } else if (airId.includes('wc')) {
-        if (document.getElementById(airId).innerHTML == "on") {
-            var wc_result = confirm(`確定要關閉冰水閥?`);
-            if (wc_result) {
-                document.getElementById(airId).innerHTML = "off"
-                return 0;
-            } else {
-                return 1;
-            }
-
-        } else {
-            document.getElementById(airId).innerHTML = "on"
-            return 1;
-        }
+    var press_result = confirm(`確定要修改壓力?`);
+    if (press_result) {
+        let value = document.getElementById('modPress').value;
+        var msg = `{"sv":` + value + "}";
+        console.log(msg);
+        message = new Paho.MQTT.Message(msg);
+        message.destinationName = "WPconf";
+        mqtt.send(message);
+        document.getElementById('modPress').value = ""; //清空輸入框
+    } else {
+        document.getElementById('modPress').value = ""; //清空輸入框
+        return;
     }
+    return false;
+
 }
-
-
-
 
 
 
