fa2faa2e8688730e0fb008c50a9c15491fc2d802
amber-ii <lube8262@gmail.com>
2021-12-22 14:23:39 +0800

    節電量null改0

--- data/water/waterByDate.sql
+++ data/water/waterByDate.sql
@@ -3,7 +3,8 @@ cast(round(Press,2) as numeric(5,2))as Press,
 cast(round(I1sp,2) as numeric(5,2))as I1sp,
 cast(round(I2sp,2) as numeric(5,2))as I2sp,
 (CASE WHEN [I1sp] > 0 THEN (1-POWER([I1sp]/60,3))*15*746/1000
-      WHEN [I2sp] > 0 THEN (1-POWER([I2sp]/60,3))*10*746/1000 END) as 'SaveAmount'
+      WHEN [I2sp] > 0 THEN (1-POWER([I2sp]/60,3))*10*746/1000
+      ELSE 0 END) as 'SaveAmount'
 from  [KEP].[dbo].[CH1WaterProjDB] where (convert(varchar(100),time, 126)) LIKE  '%:00:%' )as A
 where A.Date between  @startDate and @endDate order by (A.Date) asc
 
