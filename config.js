92ca2e473e82eba7c4fb7480add675fdc2c36623
amber-ii <lube8262@gmail.com>
2022-01-12 15:19:46 +0800

    新增sql server lims

--- config.js
+++ config.js
@@ -5,8 +5,21 @@ const assert = require('assert');
 
 dotenv.config();
 
-const {PORT, HOST, HOST_URL, SQL_USER, SQL_PASSWORD, SQL_DATABASE, SQL_SERVER} = process.env;
-
+const {
+    PORT,
+    HOST,
+    HOST_URL,
+    SQL_USER,
+    SQL_PASSWORD,
+    SQL_DATABASE,
+    SQL_SERVER,
+    SQL_DATABASE_LIMS,
+    SQL_SERVER_LIMS,
+    SQL_USER_HRM,
+    SQL_PASSWORD_HRM,
+    SQL_DATABASE_HRM,
+    SQL_SERVER_HRM,
+} = process.env
 const sqlEncrypt = process.env.SQL_ENCRYPT === 'true';
 
 assert(PORT, 'PORT is require');
@@ -23,7 +36,27 @@ module.exports = {
         password: SQL_PASSWORD,
         options: {
             encrypt: sqlEncrypt,
-            enableArithAbort: true
+            enableArithAbort: true,
         },
     },
-};
+    sql2: {
+        server: SQL_SERVER_LIMS,
+        database: SQL_DATABASE_LIMS,
+        user: SQL_USER,
+        password: SQL_PASSWORD,
+        options: {
+            encrypt: sqlEncrypt,
+            enableArithAbort: true,
+        },
+    },
+    sql_HRM: {
+        server: SQL_SERVER_HRM,
+        database: SQL_DATABASE_HRM,
+        user: SQL_USER_HRM,
+        password: SQL_PASSWORD_HRM,
+        options: {
+            encrypt: sqlEncrypt,
+            enableArithAbort: true,
+        },
+    },
+}
