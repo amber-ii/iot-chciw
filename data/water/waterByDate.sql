9617c8e8a84d224492a9d38dd93d84d1b93ea526
amber-ii <lube8262@gmail.com>
2022-01-12 15:17:23 +0800

    sql format --> convert

--- data/water/waterByDate.sql
+++ data/water/waterByDate.sql
@@ -1,10 +1,12 @@
-select Date,Press,I1sp,I2sp,cast(round(SaveAmount,3) as numeric(20,3))as 'SaveAmount' from (select format(time,'yyyy-MM-dd  HH:mm') as Date,
+select Date,Press,I1sp,I2sp,cast(round(SaveAmount,3) as numeric(20,3)) as 'SaveAmount' from 
+(select convert(varchar(16), time, 120) as Date,
 cast(round(Press,2) as numeric(5,2))as Press,
 cast(round(I1sp,2) as numeric(5,2))as I1sp,
 cast(round(I2sp,2) as numeric(5,2))as I2sp,
 (CASE WHEN [I1sp] > 0 THEN (1-POWER([I1sp]/60,3))*15*746/1000
       WHEN [I2sp] > 0 THEN (1-POWER([I2sp]/60,3))*10*746/1000
-      ELSE 0 END) as 'SaveAmount'
-from  [KEP].[dbo].[CH1WaterProjDB] where (convert(varchar(100),time, 126)) LIKE  '%:00:%' )as A
+      ELSE 0
+      END) as 'SaveAmount'
+from [KEP].[dbo].[CH1WaterProjDB] where (convert(varchar,time, 126)) LIKE  '%:00:%' )as A
 where A.Date between  @startDate and @endDate order by (A.Date) asc
 
--- data/water/waterChart30.sql
+++ data/water/waterChart30.sql
@@ -1,5 +1,8 @@
-select Date, cast(round(SaveAmount,2) as numeric(30,2))as 'SaveAmount' from  (select top 31 format(time,'yyyy-MM-dd') as 'Date',
+select Date, cast(round(SaveAmount,2) as numeric(20,2))as 'SaveAmount' 
+from (select top 31 convert(varchar(10),time,126) as 'Date',
 sum((CASE WHEN [I1sp] > 0 THEN (1-POWER([I1sp]/60,3))*15*746/1000
       WHEN [I2sp] > 0 THEN (1-POWER([I2sp]/60,3))*10*746/1000 END)) as 'SaveAmount'
-from  [KEP].[dbo].[CH1WaterProjDB] where (convert(varchar(100),time, 126)) LIKE '%:00:%'group by format(time,'yyyy-MM-dd') 
-order by format(time,'yyyy-MM-dd') desc)as A where Date<> format(GETDATE(),'yyyy-MM-dd')order by Date asc
+from  [KEP].[dbo].[CH1WaterProjDB] where (convert(varchar,time,126)) LIKE '%:00:%' 
+group by convert(varchar(10),time,126)
+order by convert(varchar(10),time,126) desc)as A where Date<> convert(varchar(10),GetDate(),126) 
+order by Date asc
