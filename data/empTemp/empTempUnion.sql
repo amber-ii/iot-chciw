a47de7051dd29b61b5d687347c404a0d17d22014
amber-ii <lube8262@gmail.com>
2022-02-17 11:44:21 +0800

    體溫一度改兩度

--- data/empTemp/empTempUnion.sql
+++ data/empTemp/empTempUnion.sql
@@ -1,8 +1,8 @@
-IF datepart(Hour,getdate()) = 9 
-(select T2.No '工號', T2.Dep '部門' ,
+IF datepart(Hour,getdate()) =9 
+(select  T2.No '工號',T2.Dep '部門' ,
 T2.Name'姓名'
-,cast(round(T1.Temp,2) as numeric(10,2)) as '體溫' 
-,cast(round(T1.AvgTemp,2) as numeric(10,2)) as '七天均溫' 
+,cast(round(T1.Temp,1) as numeric(10,1)) as '體溫' 
+,cast(round(T1.AvgTemp,1) as numeric(10,1)) as '七天均溫' 
 from (SELECT 
 A.[CardID]
 ,convert(varchar(18),B.TIME,120)'Date'
@@ -27,10 +27,10 @@ and right(convert(varchar(18),B.TIME,120),7)
 between '00:00:0' 
 and '09:00:0'
 group by A.[CardID],convert(varchar(18),B.TIME,120),B.[MSC]
-having (max(B.Temp) - avg(A.maxt)) > 1)as T1 join 
+having (max(B.Temp) - avg(A.maxt)) >= 2)as T1 join 
 
 (SELECT distinct Ca.CardNo as 'CardNo', Em.Code 'No', Em.CnName 'Name', 
-De.ShortName 'Dep' FROM 
+De.Name 'Dep' FROM 
 OpenDataSource('SQLOLEDB','server=192.168.1.182;uid=hrm;pwd=Jack@1021').[HRMDB].[dbo].[Card] As Ca 
 inner join 
 OpenDataSource('SQLOLEDB','server=192.168.1.182;uid=hrm;pwd=Jack@1021').[HRMDB].[dbo].[Employee] as Em 
@@ -42,10 +42,10 @@ where
  Ca.RevokeTypeId = '-1' and Ca.UseTypeId = 'UseType_001')as T2
 on T1.[CardID] = T2.CardNo)
 IF datepart(Hour,getdate()) = 16
-(select T2.No '工號', T2.Dep '部門' ,
+(select T2.No '工號',T2.Dep '部門' ,
 T2.Name'姓名'
-,cast(round(T1.Temp,2) as numeric(10,2)) as '體溫' 
-,cast(round(T1.AvgTemp,2) as numeric(10,2)) as '七天均溫' 
+,cast(round(T1.Temp,1) as numeric(10,1)) as '體溫' 
+,cast(round(T1.AvgTemp,1) as numeric(10,1)) as '七天均溫' 
 from (SELECT 
 A.[CardID]
 ,convert(varchar(18),B.TIME,120)'Date'
@@ -67,13 +67,13 @@ where convert(varchar(10),B.TIME,111)
 between convert(varchar(10),getdate(),111) 
 and convert(varchar(10),getdate(),111)
 and right(convert(varchar(18),B.TIME,120),7)
-between '09:00:0' 
+between '09:01:0' 
 and '16:30:0'
 group by A.[CardID],convert(varchar(18),B.TIME,120),B.[MSC]
-having (max(B.Temp) - avg(A.maxt)) > 1)as T1 join 
+having (max(B.Temp) - avg(A.maxt)) >= 2)as T1 join 
 
 (SELECT distinct Ca.CardNo as 'CardNo', Em.Code 'No', Em.CnName 'Name', 
-De.ShortName 'Dep' FROM 
+De.Name 'Dep' FROM 
 OpenDataSource('SQLOLEDB','server=192.168.1.182;uid=hrm;pwd=Jack@1021').[HRMDB].[dbo].[Card] As Ca 
 inner join 
 OpenDataSource('SQLOLEDB','server=192.168.1.182;uid=hrm;pwd=Jack@1021').[HRMDB].[dbo].[Employee] as Em 
@@ -86,10 +86,10 @@ where
 on T1.[CardID] = T2.CardNo)
 
 IF datepart(Hour,getdate()) = 20
-(select T2.No '工號', T2.Dep '部門' ,
+(select T2.No '工號',T2.Dep '部門' ,
 T2.Name'姓名'
-,cast(round(T1.Temp,2) as numeric(10,2)) as '體溫' 
-,cast(round(T1.AvgTemp,2) as numeric(10,2)) as '七天均溫' 
+,cast(round(T1.Temp,1) as numeric(10,1)) as '體溫' 
+,cast(round(T1.AvgTemp,1) as numeric(10,1)) as '七天均溫' 
 from (SELECT 
 A.[CardID]
 ,convert(varchar(18),B.TIME,120)'Date'
@@ -111,13 +111,13 @@ where convert(varchar(10),B.TIME,111)
 between convert(varchar(10),getdate(),111) 
 and convert(varchar(10),getdate(),111)
 and right(convert(varchar(18),B.TIME,120),7)
-between '16:30:0' 
+between '16:31:0' 
 and '20:30:0'
 group by A.[CardID],convert(varchar(18),B.TIME,120),B.[MSC]
-having (max(B.Temp) - avg(A.maxt)) > 1)as T1 join 
+having (max(B.Temp) - avg(A.maxt)) >= 2)as T1 join 
 
 (SELECT distinct Ca.CardNo as 'CardNo', Em.Code 'No', Em.CnName 'Name', 
-De.ShortName 'Dep' FROM 
+De.Name 'Dep' FROM 
 OpenDataSource('SQLOLEDB','server=192.168.1.182;uid=hrm;pwd=Jack@1021').[HRMDB].[dbo].[Card] As Ca 
 inner join 
 OpenDataSource('SQLOLEDB','server=192.168.1.182;uid=hrm;pwd=Jack@1021').[HRMDB].[dbo].[Employee] as Em 
