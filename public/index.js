2d9ea5a453936957becb45dc8ce919787738e645
amber-ii <lube8262@gmail.com>
2022-03-14 10:51:56 +0800

    add note

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
-$('.include').each(function() {
-    // eslint-disable-next-line
-    if (!!$(this).attr('file')) {
-        var $includeObj = $(this);
-        $(this).load($(this).attr('file'), function(html) {
-            $includeObj.after(html).remove(); //載入的檔案內容寫入到當前標籤後面並移除當前標籤
-        });
-    }
-});
+// $('.include').each(function() {
+//     // eslint-disable-next-line
+//     if (!!$(this).attr('file')) {
+//         var $includeObj = $(this);
+//         $(this).load($(this).attr('file'), function(html) {
+//             $includeObj.after(html).remove(); //載入的檔案內容寫入到當前標籤後面並移除當前標籤
+//         });
+//     }
+// });
