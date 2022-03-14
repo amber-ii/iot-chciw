ae38304b11fdf8372f7c3c5fcfc7acd34ea8077d
amber-ii <lube8262@gmail.com>
2022-03-14 10:49:17 +0800

    modify days

--- data/toc/tocChart1440.sql
+++ data/toc/tocChart1440.sql
@@ -1,5 +1,10 @@
-select A.id,A.Date,cast(round(A25TOC,2) as numeric(5,2))as A25TOC from (SELECT TOP 40 [id]
-,format(Time,'MM-dd HH:mm')as Date
+-- select A.id,A.Date,cast(round(A25TOC,2) as numeric(5,2))as A25TOC from (SELECT TOP 60 [id]
+-- ,format(Time,'MM-dd HH:mm')as Date
+-- ,[MQTTproject_A25TOC_A25TOC_VALUE]as A25TOC
+-- FROM [KEP].[dbo].[A2503DCS] order by id desc)As A order by A.id asc;
+
+select A.id,A.Date,cast(round(A25TOC,2) as numeric(5,2))as A25TOC from (SELECT TOP 180 [id]
+,right(convert(varchar(16),Time,120),11)as Date
 ,[MQTTproject_A25TOC_A25TOC_VALUE]as A25TOC
 FROM [KEP].[dbo].[A2503DCS] order by id desc)As A order by A.id asc;
 
--- data/toc/tocbyDate.sql
+++ data/toc/tocbyDate.sql
@@ -3,7 +3,7 @@
 -- ,[MQTTproject_A25TOC_A25TOC_VALUE]AS A25TOC
 -- FROM [KEP].[dbo].[A2503DCS]  where (convert(varchar(100),time, 126)) LIKE  '%00:00:%')AS A where A.Date between  @startDate and @endDate order by (A.Date) asc;
 
-SELECT FORMAT(A.time, 'yyyy-MM-dd HH:mm')AS Date, cast(round(A25TOC,2) as numeric(5,2))AS A25TOC from (SELECT [id]
+SELECT convert(varchar(16),A.Time,120)AS Date, cast(round(A25TOC,2) as numeric(5,2))AS A25TOC from (SELECT [id]
 ,time
 ,[MQTTproject_A25TOC_A25TOC_VALUE]AS A25TOC
 FROM [KEP].[dbo].[A2503DCS])AS A where A.time between  @startDate and DATEADD(day,1,@endDate) order by (A.time) asc;
