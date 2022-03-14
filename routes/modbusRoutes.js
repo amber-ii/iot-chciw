d4f5789d52fb9c8a40f2de7d6a1ab515ae06692c
amber-ii <lube8262@gmail.com>
2022-03-14 10:50:58 +0800

    create searchByTopic

--- routes/modbusRoutes.js
+++ routes/modbusRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const controller = require('../controllers/modbusController')
 const router = express.Router()
 
-router.get('/get/all', controller.getAllRecord)
+// router.get('/get/all', controller.getAllRecord)
 router.get('/get/week', controller.getThisWeek)
-router.get('/get/month', controller.getThisMonth)
+// router.get('/get/month', controller.getThisMonth)
+router.get('/topic/:topics', controller.getByTopicName)
+
 
 module.exports = {
     routes: router,
