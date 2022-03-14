6f92aef9820ceeaf51a225159055f952f888f8bc
amber-ii <lube8262@gmail.com>
2022-03-14 10:38:22 +0800

    delete month

--- controllers/modbusController.js
+++ controllers/modbusController.js
@@ -6,8 +6,7 @@ Date.prototype.addDays = function(days) {
 }
 
 let today = new Date()
-let week = new Date(today.addDays(-7))
-let months = new Date(today.addDays(-23))
+let week = new Date(today.addDays(7))
 
 const getAllRecord = async(req, res, next) => {
     if (req.user.permission == 1) {
@@ -25,7 +24,8 @@ const getAllRecord = async(req, res, next) => {
 const getThisWeek = async(req, res, next) => {
     if (req.user.permission == 1) {
         try {
-            const posts = await Modbus.find({ breakDate: { $lt: week } }).sort({ breakDate: -1 })
+            // const posts = await Modbus.find().sort({ breakDate: -1 })
+            const posts = await Modbus.find({ breakDate: { $gte: week } }).sort({ breakDate: -1 })
             res.json(posts)
         } catch (err) {
             res.json({ message: err })
@@ -38,7 +38,7 @@ const getThisWeek = async(req, res, next) => {
 const getThisMonth = async(req, res, next) => {
     if (req.user.permission == 1) {
         try {
-            const posts = await Modbus.find({ breakDate: { $lt: months } }).sort({ breakDate: -1 })
+            const posts = await Modbus.find({ breakDate: { $lte: months } }).sort({ breakDate: -1 })
             res.json(posts)
         } catch (err) {
             res.json({ message: err })
@@ -46,6 +46,25 @@ const getThisMonth = async(req, res, next) => {
     } else {
         res.sendFile(`${process.cwd()}/public/404.html`)
     }
+}
+
+const getByTopicName = async(req, res, next) => {
+    if (req.user.permission == 1) {
+        try {
+            // let posts = await Modbus.find({ topic: { $regex: req.params.topics } })
+            // let posts = await Modbus.find({ topic: req.params.topics }).find({ breakDate: { $gte: week } })
+            let posts = await Modbus.find({ topic: req.params.topics, breakDate: { $gte: week } }).sort({ breakDate: -1 })
+
+            console.log('我是資料庫回傳結果' + posts)
+            res.json(posts)
+        } catch (err) {
+            console.log("有錯")
+            res.json({ message: err })
+        }
+    } else {
+        res.sendFile(`${process.cwd()}/public/404.html`)
+    }
+
 }
 
 // const getThreeMonth = async(req, res, next) => {
@@ -82,5 +101,6 @@ module.exports = {
     getAllRecord,
     getThisWeek,
     getThisMonth,
+    getByTopicName
     // getSixMonth,
 }
