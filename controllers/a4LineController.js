c12c5c8f488fabccf5c0a1fded300a3e3678a9ff
amber-ii <lube8262@gmail.com>
2022-03-31 08:51:10 +0800

    create a4LineWarning

---
+++ controllers/a4LineController.js
@@ -0,0 +1,34 @@
+'use strict'
+const data = require('../data/a4Line')
+const lineNotify = require('line-notify-nodejs')('TNL91MqDslu9PPIsVVIipviRC4K1o7w3znWsXQ4f9ZQ')
+
+const getEvent = async (req, res, next) => {
+  try {
+    let result = await data.getEvent()
+    result = await JSON.stringify(result)
+      .replace(/[[]/g, '')
+      .replace(/[\]]/g, '')
+      .replace(/[_VALUE]/g, '')
+    result = JSON.parse(result)
+    for (let id in result) {
+      if (result[id] == true) {
+        lineNotify
+          .notify({
+            message: '\n\n' + '⚡ ' + id,
+          })
+          .then(() => {
+            console.log('a4-1自動水洗警報發送')
+          })
+      }
+    }
+  } catch (error) {
+  }
+}
+
+setInterval(() => {
+  getEvent()
+},60000)
+
+module.exports = {
+  getEvent,
+}
