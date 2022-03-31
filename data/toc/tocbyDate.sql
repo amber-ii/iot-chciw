f5a1c7b73a6d49d7fa39b6ec3f93349e424ce091
amber-ii <lube8262@gmail.com>
2022-03-31 13:11:02 +0800

    add  getByTocValue

--- data/toc/tocbyDate.sql
+++ data/toc/tocbyDate.sql
@@ -2,7 +2,7 @@ IF @tocValue IS NOT NULL
 SELECT convert(varchar(16),A.Time,120)AS Date, cast(round(A25TOC,2) as numeric(5,2))AS A25TOC from (SELECT [id]
 ,time
 ,[MQTTproject_A25TOC_A25TOC_VALUE]AS A25TOC
-FROM [KEP].[dbo].[A2503DCS])AS A where A.time between  @startDate and DATEADD(day,1,@endDate) and A25TOC >= @tocValue order by (A.time) asc;
+FROM [KEP].[dbo].[A2503DCS])AS A where A.time between  @startDate and DATEADD(day,1,@endDate) and (A25TOC >= @tocValue) order by (A.time) asc;
 
 IF @tocValue IS NULL
 SELECT convert(varchar(16),A.Time,120)AS Date, cast(round(A25TOC,2) as numeric(5,2))AS A25TOC from (SELECT [id]
